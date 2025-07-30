110ddb4e2f9061901154fc9e04abca0f86c73a52
William Cavalcanti <166062273+WilliamssCavalcanti@users.noreply.github.com>
2025-07-30 08:02:20 -0300

    Update food.py

--- food.py
+++ food.py
@@ -5,15 +5,15 @@ import random
 class Food(Turtle):
 
     def __init__(self):
-        super().__init__()
-        self.shape('circle')
-        self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color('blue')
-        self.speed('fastest')
-        self.refresh()
+        super().__init__()    # Inicializa como uma Turtle
+        self.shape('circle')     # Define o formato como um círculo
+        self.penup()             # Levanta o "lápis" (Não deixa rastro)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)    # Diminui o tamanho do círculo
+        self.color('blue')     # Cor da comida (azul)
+        self.speed('fastest')    # Cria a comida rapidamente
+        self.refresh()     # Posiciona a primeira comida em um local aleatório
 
-    def refresh(self):
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
-        self.goto(random_x, random_y)
+    def refresh(self): # Gera nova posição aleatória para a comida na tela
+        random_x = random.randint(-280, 280)   # Eixo X aleatório dentro da tela
+        random_y = random.randint(-280, 280)   # Eixo Y aleatório dentro da tela
+        self.goto(random_x, random_y)          # Movea comida para a nova posição
